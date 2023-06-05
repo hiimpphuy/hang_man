72aae3cecdbb670a7eb9b7ae2437357d1b15bba4
phuy <135548241+hiimpphuy@users.noreply.github.com>
2023-06-06 01:05:26 +0700

    Add files via upload

--- addlib.py
+++ addlib.py
@@ -1,30 +1,98 @@
-import os
 import random
-import time
-import keyboard
+import os
 
-treasure_x = random.randint(0,2)
-treasure_y = random.randint(0,2)
-row1= [" ", " ", " "]
-row2= [" ", " ", " "]
-row3= [" ", " ", " "]
-treasure_map = [row1,row2,row3]
+#region initial_resource
+lst_word = ["apple", "watermelon", "cherry", "pineapple","Grapes"]
+word = random.choice(lst_word)
+map = ["_" for i in word]
+life = 5
+lst_check = [i for i in word]
+stm0 = "   ___________\
+ \n    |/      | \
+ \n    |      (_) \
+ \n    |      \|/ \
+ \n    |       | \
+ \n    |      / \ \
+ \n    | \
+ \n ___|___"
+
+stm1 = "   ___________\
+ \n    |/      | \
+ \n    |      (_) \
+ \n    |      \|/ \
+ \n    |       | \
+ \n    |      \
+ \n    | \
+ \n ___|___"
+
+stm2 = "   ___________\
+ \n    |/      | \
+ \n    |      (_) \
+ \n    |      \|/ \
+ \n    |       \
+ \n    |      \
+ \n    | \
+ \n ___|___"
+
+stm3 = "   ___________\
+ \n    |/      | \
+ \n    |      (_) \
+ \n    |      \
+ \n    |       \
+ \n    |      \
+ \n    | \
+ \n ___|___"
+
+stm4 = "   ___________\
+ \n    |/      | \
+ \n    |      \
+ \n    |      \
+ \n    |      \
+ \n    |      \
+ \n    | \
+ \n ___|___"
+
+stickman = [stm0,stm1,stm2,stm3,stm4]
+#endregion
+
+#region func-game
+def check_word(letter_input,word,life):
+    if letter_input in word:
+        changeLettertoMap(letter_input,map,lst_check)
+    else:
+        life -= 1
+        drawstickman(life,stickman)
+
+def changeLettertoMap(letter_input,map,lst_check):
+    changeLettertoMap.has_been_called = True
+    for i in range(0,len(lst_check)):
+        if letter_input == lst_check[i]:
+            map[i] = letter_input
+def drawstickman(life,stickman):
+    drawstickman.has_been_called = True
+    print(stickman[life])
+if __name__ == '__main__':
+    drawstickman.has_been_called = False
+#endregion
+
+#region MG
 
 while True:
-    os.system('clear')
-    print(f"{row1}\n{row2}\n{row3}")
-    position = input("Where the treasure ?? ")
-    if position == "q":
+    os.system("clear")
+    print(*map)
+    letter_input = input("Guess it: ")
+    check_word(letter_input,word,life)
+    if drawstickman.has_been_called:
+        drawstickman.has_been_called = False
+        life -= 1
+    if "".join(map) == word:
+        print(word)
+        print("Congratulations!!")
         break
-    horizonal = int(position[0])
-    vertical  = int(position[1])
-    treasure_map[horizonal-1][vertical-1] = "X"
-    if horizonal-1 == treasure_x and vertical-1 == treasure_y:
-        treasure_map[horizonal - 1][vertical - 1] = "@"
-        print("You find the treasure")
-        print(f"{row1}\n{row2}\n{row3}")
-        keyboard.wait('space')
+    if life == 0:
+        print("Your stickman is dead. Game Over!!")
         break
-    else:
-        print("Try again. You almost get it!!")
-    time.sleep(2)
+#endregion
+
+
+
